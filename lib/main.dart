8461a055460c4885ead76b272af59675bd996c4d
unknown <shrestha8502@gmail.com>
2024-04-26 15:55:53 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:practice_architecture/features/trivia_page/presentation/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
